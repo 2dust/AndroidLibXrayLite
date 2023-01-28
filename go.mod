cb35200fbeacecc6d88bbc0090754a7a630a5f64
2dust <31833384+2dust@users.noreply.github.com>
2023-01-28 15:13:13 +0800

    delay test http to https

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/2dust/AndroidLibXrayLite
 go 1.19
 
 require (
-	github.com/xtls/xray-core v1.6.6
+	github.com/xtls/xray-core v1.7.2
 	golang.org/x/mobile v0.0.0-20221020085226-b36e6246172e
 	golang.org/x/sys v0.1.0
 )
--- libv2ray_main.go
+++ libv2ray_main.go
@@ -270,7 +270,7 @@ func measureInstDelay(ctx context.Context, inst *v2core.Instance) (int64, error)
 		Timeout:   12 * time.Second,
 	}
 
-	req, _ := http.NewRequestWithContext(ctx, "GET", "http://www.google.com/generate_204", nil)
+	req, _ := http.NewRequestWithContext(ctx, "GET", "https://www.google.com/generate_204", nil)
 	start := time.Now()
 	resp, err := c.Do(req)
 	if err != nil {
