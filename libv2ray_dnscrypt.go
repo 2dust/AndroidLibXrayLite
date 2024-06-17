85cf4c1790479e9cbddb2fceef128de3ad2f6ef6
Hamid Mahmoodi <shr.mahmoodi@gmail.com>
2024-06-17 20:05:42 +0330

    Revert "Fix dns"
    This reverts commit 0d4829ea81739f519e96e7930c2e4b27bacec029.

--- libv2ray_dnscrypt.go
+++ libv2ray_dnscrypt.go
@@ -56,7 +56,7 @@ func helper(configStr string) {
 	emptyStr := ""
 	falseBool := false
 	timeout := 60
-	configName := configStr
+	configName := "config.yml"
 
 	svcFlag := &emptyStr
 	version := &falseBool
