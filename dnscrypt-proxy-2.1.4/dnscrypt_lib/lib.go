a32bfcbd651bdcd7eee6f638defc5a3d66b5c43e
Hamid Mahmoodi <shr.mahmoodi@gmail.com>
2024-06-17 15:25:08 +0330

    Revert "test"
    This reverts commit 9c6dc4deb82047e6a8ccce15d9271ed3b0b1b1db.

--- dnscrypt-proxy-2.1.4/dnscrypt_lib/lib.go
+++ dnscrypt-proxy-2.1.4/dnscrypt_lib/lib.go
@@ -44,7 +44,7 @@ func Start(config string) (*ProxyServer, error) {
 
 	var proxy *dnscrypt_proxy.Proxy = dnscrypt_proxy.NewProxy()
 
-	if err := dnscrypt_proxy.ConfigLoad(proxy, nil); err != nil {
+	if err := dnscrypt_proxy.ConfigLoad(proxy, config); err != nil {
 		return nil, err
 	}
 	// if err := dnscrypt_proxy.PidFileCreate(); err != nil {
