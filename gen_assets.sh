3369fe05e9315bf1064721f50feb0f56958c7d3d
alphax-hue3682 <191818854+alphax-hue3682@users.noreply.github.com>
2024-12-24 15:11:36 +0330

    Update gen_assets.sh

--- gen_assets.sh
+++ gen_assets.sh
@@ -1,84 +1,79 @@
 #!/bin/bash
 
-set -o errexit
-set -o pipefail
-set -o nounset
+set -euo pipefail
 
-# Define magic variables for current file & dir
+# Set magic variables for current file & dir
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 __base="$(basename "${__file}" .sh)"
 
 DATADIR="${__dir}/data"
 
+handle_error() {
+    echo -e "Aborted, error $? in command: $BASH_COMMAND"
+    exit 1
+}
+
+trap handle_error ERR
+
+update_file() {
+    local src_file="$1"
+    local dest_file="$2"
+    
+    if [[ -e "$src_file" ]]; then
+        rm -f "$dest_file"
+        mv "$src_file" "$dest_file"
+        echo "----------> ${dest_file} updated."
+    else
+        echo "----------> ${dest_file} failed to update."
+    fi
+}
+
 compile_dat() {
     local TMPDIR
     TMPDIR=$(mktemp -d)
 
-    trap 'echo -e "Aborted, error $? in command: $BASH_COMMAND"; rm -rf "$TMPDIR"; exit 1' ERR
-
     local GEOSITE="${GOPATH}/src/github.com/v2ray/domain-list-community"
     
-    # Update or clone the geosite repository
     if [[ -d "${GEOSITE}" ]]; then
-        (cd "${GEOSITE}" && git pull)
+        cd "${GEOSITE}" && git pull
     else
         mkdir -p "${GEOSITE}"
-        git clone https://github.com/v2ray/domain-list-community.git "${GEOSITE}"
-    fi
-    
-    (cd "${GEOSITE}" && go run main.go)
-
-    # Update geosite.dat
-    if [[ -e dlc.dat ]]; then
-        rm -f "${DATADIR}/geosite.dat"
-        mv dlc.dat "${DATADIR}/geosite.dat"
-        echo "----------> geosite.dat updated."
-    else
-        echo "----------> geosite.dat failed to update."
+        cd "${GEOSITE}" && git clone https://github.com/v2ray/domain-list-community.git .
     fi
 
-    # Ensure geoip is installed
-    if [[ ! -x "${GOPATH}/bin/geoip" ]]; then
+    go run main.go
+    update_file "dlc.dat" "$DATADIR/geosite.dat"
+
+    if [[ ! -x "$GOPATH/bin/geoip" ]]; then
         go get -v -u github.com/v2ray/geoip
     fi
 
-    # Download and process GeoLite2 data
-    (cd "$TMPDIR" && {
-        curl -L -O http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country-CSV.zip
-        unzip -q GeoLite2-Country-CSV.zip
-        mkdir geoip && find . -name '*.csv' -exec mv -t ./geoip {} +
-        
-        "${GOPATH}/bin/geoip" \
-            --country=./geoip/GeoLite2-Country-Locations-en.csv \
-            --ipv4=./geoip/GeoLite2-Country-Blocks-IPv4.csv \
-            --ipv6=./geoip/GeoLite2-Country-Blocks-IPv6.csv
-    })
+    cd "$TMPDIR"
+    curl -L -O http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country-CSV.zip
+    unzip -q GeoLite2-Country-CSV.zip
+    mkdir geoip && find . -name '*.csv' -exec mv -t ./geoip {} +
 
-    # Update geoip.dat
-    if [[ -e geoip.dat ]]; then
-        rm -f "${DATADIR}/geoip.dat"
-        mv geoip.dat "${DATADIR}/geoip.dat"
-        echo "----------> geoip.dat updated."
-    else
-        echo "----------> geoip.dat failed to update."
-    fi
+    "$GOPATH/bin/geoip" \
+        --country=./geoip/GeoLite2-Country-Locations-en.csv \
+        --ipv4=./geoip/GeoLite2-Country-Blocks-IPv4.csv \
+        --ipv6=./geoip/GeoLite2-Country-Blocks-IPv6.csv
 
-    # Clean up temporary directory
+    update_file "geoip.dat" "$DATADIR/geoip.dat"
+
     rm -rf "$TMPDIR"
 }
 
 download_dat() {
     wget -qO - https://api.github.com/repos/v2ray/geoip/releases/latest \
         | grep browser_download_url | cut -d '"' -f 4 \
-        | wget -i - -O "${DATADIR}/geoip.dat"
+        | wget -i - -O "$DATADIR/geoip.dat"
 
     wget -qO - https://api.github.com/repos/v2ray/domain-list-community/releases/latest \
         | grep browser_download_url | cut -d '"' -f 4 \
-        | wget -i - -O "${DATADIR}/geosite.dat"
+        | wget -i - -O "$DATADIR/geosite.dat"
 }
 
-# Determine action based on input argument or default to download
 ACTION="${1:-download}"
 
 case $ACTION in
