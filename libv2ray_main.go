8dfb2ae0868e72bf94eeb372f03267863c305463
alphax-hue3682 <191818854+alphax-hue3682@users.noreply.github.com>
2024-12-25 12:04:16 +0330

    Update libv2ray_main.go

--- libv2ray_main.go
+++ libv2ray_main.go
@@ -248,7 +248,7 @@ CheckVersionX string
 This func will return libv2ray binding version and V2Ray version used.
 */
 func CheckVersionX() string {
-	var version = 27
+	var version = 30
 	return fmt.Sprintf("Lib v%d, Xray-core v%s", version, v2core.Version())
 }
 
