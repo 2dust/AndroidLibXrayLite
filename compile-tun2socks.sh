d8255522334e488c56fc499cd07648e3b291905d
UjuiUjuMandan <125150101+UjuiUjuMandan@users.noreply.github.com>
2025-01-11 10:28:06 +0000

    ABI parameter for compile-tun2socks.sh

--- compile-tun2socks.sh
+++ compile-tun2socks.sh
@@ -22,13 +22,36 @@ clear_tmp () {
 trap 'echo -e "Aborted, error $? in command: $BASH_COMMAND"; trap ERR; clear_tmp; exit 1' ERR INT
 install -m644 $__dir/tun2socks.mk $TMPDIR/
 
+abi_filter="all"
+if [ "$#" -eq 1 ]; then
+  case $1 in
+    arm64-v8a)
+      abi_filter="arm64-v8a"
+      ;;
+    armeabi-v7a)
+      abi_filter="armeabi-v7a"
+      ;;
+    x86_64)
+      abi_filter="x86_64"
+      ;;
+    x86)
+      abi_filter="x86"
+      ;;
+    *)
+      echo "Invalid ABI specified: $1"
+      echo "Valid options are: arm64-v8a, armeabi-v7a, x86_64, x86"
+      exit 1
+      ;;
+  esac
+fi
+
 pushd $TMPDIR
 ln -s $__dir/badvpn badvpn
 ln -s $__dir/libancillary libancillary
 $NDK_HOME/ndk-build \
 	NDK_PROJECT_PATH=. \
 	APP_BUILD_SCRIPT=./tun2socks.mk \
-	APP_ABI=all \
+	APP_ABI=$abi_filter \
 	APP_PLATFORM=android-19 \
 	NDK_LIBS_OUT=$TMPDIR/libs \
 	NDK_OUT=$TMPDIR/tmp \
