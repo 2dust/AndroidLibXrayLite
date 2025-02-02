8b81fe07d069a04462dfd904b36e1428d6873e1d
alphax-hue3682 <191818854+alphax-hue3682@users.noreply.github.com>
2025-02-02 13:29:15 +0330

    Update gen_assets.sh

--- gen_assets.sh
+++ gen_assets.sh
@@ -11,66 +11,80 @@ __base="$(basename "${__file}" .sh)"
 
 DATADIR="${__dir}/data"
 
-compile_dat () {
-    local TMPDIR
-    TMPDIR=$(mktemp -d)
+# Function to handle errors
+error_exit() {
+    echo -e "Aborted, error $? in command: $BASH_COMMAND"
+    [[ -d "$TMPDIR" ]] && rm -rf "$TMPDIR"
+    exit 1
+}
 
-    trap 'echo "Aborted, error $? in command: $BASH_COMMAND"; rm -rf $TMPDIR; exit 1' ERR
+# Compile data function
+compile_dat() {
+    TMPDIR=$(mktemp -d)
+    trap 'error_exit' ERR
 
     local GEOSITE="${GOPATH}/src/github.com/v2ray/domain-list-community"
+
+    # Clone or update the geosite repository
     if [[ -d ${GEOSITE} ]]; then
-        git -C "${GEOSITE}" pull
+        (cd "${GEOSITE}" && git pull)
     else
         git clone https://github.com/v2ray/domain-list-community.git "${GEOSITE}"
     fi
-    go run "${GEOSITE}/main.go"
+    
+    (cd "${GEOSITE}" && go run main.go)
 
+    # Update geosite.dat if dlc.dat exists
     if [[ -e "${GEOSITE}/dlc.dat" ]]; then
-        mv -f "${GEOSITE}/dlc.dat" "${DATADIR}/geosite.dat"
+        mv -f "${GEOSITE}/dlc.dat" "$DATADIR/geosite.dat"
         echo "----------> geosite.dat updated."
     else
         echo "----------> geosite.dat failed to update."
     fi
 
-    if [[ ! -x "${GOPATH}/bin/geoip" ]]; then
-        go install github.com/v2ray/geoip@latest
+    # Install geoip if not already installed
+    if [[ ! -x "$GOPATH/bin/geoip" ]]; then
+        go get -v -u github.com/v2ray/geoip
     fi
 
-    cd "${TMPDIR}"
-    curl -sL -O http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country-CSV.zip
+    cd "$TMPDIR"
+    
+    # Download and process GeoLite2 data
+    curl -L -O http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country-CSV.zip
     unzip -q GeoLite2-Country-CSV.zip
-    mkdir -p geoip && find . -name '*.csv' -exec mv -t ./geoip {} +
+    mkdir geoip && mv *.csv geoip/
 
-    "${GOPATH}/bin/geoip" \
+    "$GOPATH/bin/geoip" \
         --country=./geoip/GeoLite2-Country-Locations-en.csv \
         --ipv4=./geoip/GeoLite2-Country-Blocks-IPv4.csv \
         --ipv6=./geoip/GeoLite2-Country-Blocks-IPv6.csv
 
+    # Update geoip.dat if it exists
     if [[ -e geoip.dat ]]; then
-        mv -f geoip.dat "${DATADIR}/geoip.dat"
+        mv -f geoip.dat "$DATADIR/geoip.dat"
         echo "----------> geoip.dat updated."
     else
         echo "----------> geoip.dat failed to update."
     fi
-
-    rm -rf "${TMPDIR}"
+    
+    trap - ERR  # Disable error trap
 }
 
-download_dat () {
-    local geoip_url
-    local geosite_url
+# Download data function
+download_dat() {
+    wget -qO - https://api.github.com/repos/dyhkwong/v2ray-geoip/releases/latest \
+        | jq -r .assets[].browser_download_url | grep geoip.dat \
+        | xargs wget -O "$DATADIR/geoip.dat"
 
-    geoip_url=$(wget -qO - https://api.github.com/repos/dyhkwong/v2ray-geoip/releases/latest | jq -r '.assets[].browser_download_url' | grep geoip.dat)
-    wget -q "${geoip_url}" -O "${DATADIR}/geoip.dat"
-
-    geosite_url=$(wget -qO - https://api.github.com/repos/v2ray/domain-list-community/releases/latest | jq -r '.assets[].browser_download_url' | grep geosite.dat)
-    wget -q "${geosite_url}" -O "${DATADIR}/geosite.dat"
+    wget -qO - https://api.github.com/repos/v2ray/domain-list-community/releases/latest \
+        | grep browser_download_url | cut -d '"' -f 4 \
+        | xargs wget -O "$DATADIR/geosite.dat"
 }
 
+# Main execution logic
 ACTION="${1:-download}"
 
-case "${ACTION}" in
+case $ACTION in
     "download") download_dat ;;
     "compile") compile_dat ;;
-    *) echo "Unknown action: ${ACTION}" ;;
 esac
