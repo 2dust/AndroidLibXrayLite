55f25363f28bde6d6ec986e79492edddc62d708e
alphax-hue3682 <191818854+alphax-hue3682@users.noreply.github.com>
2025-02-03 10:40:43 +0330

    Update libv2ray_main.go

--- libv2ray_main.go
+++ libv2ray_main.go
@@ -1,322 +1,314 @@
 package libv2ray
 
 import (
-	"context"
-	"errors"
-	"fmt"
-	"io"
-	"log"
-	"net"
-	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
-	"sync"
-	"time"
+    "context"
+    "errors"
+    "fmt"
+    "io"
+    "log"
+    "net"
+    "net/http"
+    "os"
+    "path/filepath"
+    "strings"
+    "sync"
+    "time"
 
-	mobasset "golang.org/x/mobile/asset"
+    mobasset "golang.org/x/mobile/asset"
 
-	v2net "github.com/xtls/xray-core/common/net"
-	v2filesystem "github.com/xtls/xray-core/common/platform/filesystem"
-	"github.com/xtls/xray-core/common/serial"
-	v2core "github.com/xtls/xray-core/core"
-	v2stats "github.com/xtls/xray-core/features/stats"
-	v2serial "github.com/xtls/xray-core/infra/conf/serial"
-	_ "github.com/xtls/xray-core/main/distro/all"
-	v2internet "github.com/xtls/xray-core/transport/internet"
+    v2net "github.com/xtls/xray-core/common/net"
+    v2filesystem "github.com/xtls/xray-core/common/platform/filesystem"
+    "github.com/xtls/xray-core/common/serial"
+    v2core "github.com/xtls/xray-core/core"
+    v2stats "github.com/xtls/xray-core/features/stats"
+    v2serial "github.com/xtls/xray-core/infra/conf/serial"
+    _ "github.com/xtls/xray-core/main/distro/all"
+    v2internet "github.com/xtls/xray-core/transport/internet"
 
-	v2applog "github.com/xtls/xray-core/app/log"
-	v2commlog "github.com/xtls/xray-core/common/log"
+    v2applog "github.com/xtls/xray-core/app/log"
+    v2commlog "github.com/xtls/xray-core/common/log"
 )
 
 const (
-	v2Asset     = "xray.location.asset"
-	xudpBaseKey = "xray.xudp.basekey"
+    v2Asset     = "xray.location.asset"
+    xudpBaseKey = "xray.xudp.basekey"
 )
 
-/*
-V2RayPoint V2Ray Point Server
-This is territory of Go, so no getter and setters!
-*/
+// V2RayPoint V2Ray Point Server
 type V2RayPoint struct {
-	SupportSet   V2RayVPNServiceSupportsSet
-	statsManager v2stats.Manager
+    SupportSet   V2RayVPNServiceSupportsSet
+    statsManager v2stats.Manager
 
-	dialer    *ProtectedDialer
-	v2rayOP   sync.Mutex
-	closeChan chan struct{}
+    dialer    *ProtectedDialer
+    v2rayOP   sync.Mutex
+    closeChan chan struct{}
 
-	Vpoint    *v2core.Instance
-	IsRunning bool
+    Vpoint    *v2core.Instance
+    IsRunning bool
 
-	DomainName           string
-	ConfigureFileContent string
-	AsyncResolve         bool
+    DomainName           string
+    ConfigureFileContent string
+    AsyncResolve         bool
 }
 
-/*V2RayVPNServiceSupportsSet To support Android VPN mode*/
+// V2RayVPNServiceSupportsSet To support Android VPN mode
 type V2RayVPNServiceSupportsSet interface {
-	Setup(Conf string) int
-	Prepare() int
-	Shutdown() int
-	Protect(int) bool
-	OnEmitStatus(int, string) int
+    Setup(Conf string) int
+    Prepare() int
+    Shutdown() int
+    Protect(int) bool
+    OnEmitStatus(int, string) int
 }
 
-/*RunLoop Run V2Ray main loop
- */
+// RunLoop Run V2Ray main loop
 func (v *V2RayPoint) RunLoop(prefIPv6 bool) (err error) {
-	v.v2rayOP.Lock()
-	defer v.v2rayOP.Unlock()
-	//Construct Context
+    v.v2rayOP.Lock()
+    defer v.v2rayOP.Unlock()
 
-	if !v.IsRunning {
-		v.closeChan = make(chan struct{})
-		v.dialer.PrepareResolveChan()
-		go func() {
-			select {
-			// wait until resolved
-			case <-v.dialer.ResolveChan():
-				// shutdown VPNService if server name can not reolved
-				if !v.dialer.IsVServerReady() {
-					log.Println("vServer cannot resolved, shutdown")
-					v.StopLoop()
-					v.SupportSet.Shutdown()
-				}
+    if !v.IsRunning {
+        v.closeChan = make(chan struct{})
+        v.dialer.PrepareResolveChan()
+        go func() {
+            select {
+            // wait until resolved
+            case <-v.dialer.ResolveChan():
+                // shutdown VPNService if server name can not be resolved
+                if !v.dialer.IsVServerReady() {
+                    log.Println("vServer cannot be resolved, shutdown")
+                    v.StopLoop()
+                    v.SupportSet.Shutdown()
+                }
 
-			// stop waiting if manually closed
-			case <-v.closeChan:
-			}
-		}()
+            // stop waiting if manually closed
+            case <-v.closeChan:
+            }
+        }()
 
-		if v.AsyncResolve {
-			go func() {
-				v.dialer.PrepareDomain(v.DomainName, v.closeChan, prefIPv6)
-				close(v.dialer.ResolveChan())
-			}()
-		} else {
-			v.dialer.PrepareDomain(v.DomainName, v.closeChan, prefIPv6)
-			close(v.dialer.ResolveChan())
-		}
+        if v.AsyncResolve {
+            go func() {
+                v.dialer.PrepareDomain(v.DomainName, v.closeChan, prefIPv6)
+                close(v.dialer.ResolveChan())
+            }()
+        } else {
+            v.dialer.PrepareDomain(v.DomainName, v.closeChan, prefIPv6)
+            close(v.dialer.ResolveChan())
+        }
 
-		err = v.pointloop()
-	}
-	return
+        err = v.pointloop()
+    }
+    return
 }
 
-/*StopLoop Stop V2Ray main loop
- */
+// StopLoop Stop V2Ray main loop
 func (v *V2RayPoint) StopLoop() (err error) {
-	v.v2rayOP.Lock()
-	defer v.v2rayOP.Unlock()
-	if v.IsRunning {
-		close(v.closeChan)
-		v.shutdownInit()
-		v.SupportSet.OnEmitStatus(0, "Closed")
-	}
-	return
+    v.v2rayOP.Lock()
+    defer v.v2rayOP.Unlock()
+    if v.IsRunning {
+        close(v.closeChan)
+        v.shutdownInit()
+        v.SupportSet.OnEmitStatus(0, "Closed")
+    }
+    return
 }
 
-// Delegate Funcation
+// QueryStats queries statistics
 func (v V2RayPoint) QueryStats(tag string, direct string) int64 {
-	if v.statsManager == nil {
-		return 0
-	}
-	counter := v.statsManager.GetCounter(fmt.Sprintf("outbound>>>%s>>>traffic>>>%s", tag, direct))
-	if counter == nil {
-		return 0
-	}
-	return counter.Set(0)
+    if v.statsManager == nil {
+        return 0
+    }
+    counter := v.statsManager.GetCounter(fmt.Sprintf("outbound>>>%s>>>traffic>>>%s", tag, direct))
+    if counter == nil {
+        return 0
+    }
+    return counter.Set(0)
 }
 
 func (v *V2RayPoint) shutdownInit() {
-	v.IsRunning = false
-	v.Vpoint.Close()
-	v.Vpoint = nil
-	v.statsManager = nil
+    v.IsRunning = false
+    v.Vpoint.Close()
+    v.Vpoint = nil
+    v.statsManager = nil
 }
 
 func (v *V2RayPoint) pointloop() error {
-	log.Println("loading core config")
-	config, err := v2serial.LoadJSONConfig(strings.NewReader(v.ConfigureFileContent))
-	if err != nil {
-		log.Println(err)
-		return err
-	}
+    log.Println("loading core config")
+    config, err := v2serial.LoadJSONConfig(strings.NewReader(v.ConfigureFileContent))
+    if err != nil {
+        log.Println(err)
+        return err
+    }
 
-	log.Println("new core")
-	v.Vpoint, err = v2core.New(config)
-	if err != nil {
-		v.Vpoint = nil
-		log.Println(err)
-		return err
-	}
-	v.statsManager = v.Vpoint.GetFeature(v2stats.ManagerType()).(v2stats.Manager)
+    log.Println("new core")
+    v.Vpoint, err = v2core.New(config)
+    if err != nil {
+        v.Vpoint = nil
+        log.Println(err)
+        return err
+    }
+    v.statsManager = v.Vpoint.GetFeature(v2stats.ManagerType()).(v2stats.Manager)
 
-	log.Println("start core")
-	v.IsRunning = true
-	if err := v.Vpoint.Start(); err != nil {
-		v.IsRunning = false
-		log.Println(err)
-		return err
-	}
+    log.Println("start core")
+    v.IsRunning = true
+    if err := v.Vpoint.Start(); err != nil {
+        v.IsRunning = false
+        log.Println(err)
+        return err
+    }
 
-	v.SupportSet.Prepare()
-	v.SupportSet.Setup("")
-	v.SupportSet.OnEmitStatus(0, "Running")
-	return nil
+    v.SupportSet.Prepare()
+    v.SupportSet.Setup("")
+    v.SupportSet.OnEmitStatus(0, "Running")
+    return nil
 }
 
 func (v *V2RayPoint) MeasureDelay(url string) (int64, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 12*time.Second)
+    ctx, cancel := context.WithTimeout(context.Background(), 12*time.Second)
 
-	go func() {
-		select {
-		case <-v.closeChan:
-			// cancel request if close called during meansure
-			cancel()
-		case <-ctx.Done():
-		}
-	}()
+    go func() {
+        select {
+        case <-v.closeChan:
+            // cancel request if close called during measure
+            cancel()
+        case <-ctx.Done():
+        }
+    }()
 
-	return measureInstDelay(ctx, v.Vpoint, url)
+    return measureInstDelay(ctx, v.Vpoint, url)
 }
 
 // InitV2Env set v2 asset path
 func InitV2Env(envPath string, key string) {
-	//Initialize asset API, Since Raymond Will not let notify the asset location inside Process,
-	//We need to set location outside V2Ray
-	if len(envPath) > 0 {
-		os.Setenv(v2Asset, envPath)
-	}
-	if len(key) > 0 {
-		os.Setenv(xudpBaseKey, key)
-	}
+    //Initialize asset API, Since Raymond Will not let notify the asset location inside Process,
+    //We need to set location outside V2Ray
+    if len(envPath) > 0 {
+        os.Setenv(v2Asset, envPath)
+    }
+    if len(key) > 0 {
+        os.Setenv(xudpBaseKey, key)
+    }
 
-	//Now we handle read, fallback to gomobile asset (apk assets)
-	v2filesystem.NewFileReader = func(path string) (io.ReadCloser, error) {
-		if _, err := os.Stat(path); os.IsNotExist(err) {
-			_, file := filepath.Split(path)
-			return mobasset.Open(file)
-		}
-		return os.Open(path)
-	}
+    //Now we handle read, fallback to gomobile asset (apk assets)
+    v2filesystem.NewFileReader = func(path string) (io.ReadCloser, error) {
+        if _, err := os.Stat(path); os.IsNotExist(err) {
+            _, file := filepath.Split(path)
+            return mobasset.Open(file)
+        }
+        return os.Open(path)
+    }
 }
 
+// MeasureOutboundDelay measures outbound delay
 func MeasureOutboundDelay(ConfigureFileContent string, url string) (int64, error) {
-	config, err := v2serial.LoadJSONConfig(strings.NewReader(ConfigureFileContent))
-	if err != nil {
-		return -1, err
-	}
+    config, err := v2serial.LoadJSONConfig(strings.NewReader(ConfigureFileContent))
+    if err != nil {
+        return -1, err
+    }
 
-	// dont listen to anything for test purpose
-	config.Inbound = nil
-	// config.App: (fakedns), log, dispatcher, InboundConfig, OutboundConfig, (stats), router, dns, (policy)
-	// keep only basic features
-	var essentialApp []*serial.TypedMessage
-	for _, app := range config.App {
-		if app.Type == "xray.app.proxyman.OutboundConfig" || app.Type == "xray.app.dispatcher.Config" || app.Type == "xray.app.log.Config" {
-			essentialApp = append(essentialApp, app)
-		}
-	}
-	config.App = essentialApp
+    // don't listen to anything for test purpose
+    config.Inbound = nil
+    // config.App: (fakedns), log, dispatcher, InboundConfig, OutboundConfig, (stats), router, dns, (policy)
+    // keep only basic features
+    var essentialApp []*serial.TypedMessage
+    for _, app := range config.App {
+        if app.Type == "xray.app.proxyman.OutboundConfig" || app.Type == "xray.app.dispatcher.Config" || app.Type == "xray.app.log.Config" {
+            essentialApp = append(essentialApp, app)
+        }
+    }
+    config.App = essentialApp
 
-	inst, err := v2core.New(config)
-	if err != nil {
-		return -1, err
-	}
+    inst, err := v2core.New(config)
+    if err != nil {
+        return -1, err
+    }
 
-	inst.Start()
-	delay, err := measureInstDelay(context.Background(), inst, url)
-	inst.Close()
-	return delay, err
+    inst.Start()
+    delay, err := measureInstDelay(context.Background(), inst, url)
+    inst.Close()
+    return delay, err
 }
 
-/*NewV2RayPoint new V2RayPoint*/
+// NewV2RayPoint creates a new V2RayPoint
 func NewV2RayPoint(s V2RayVPNServiceSupportsSet, adns bool) *V2RayPoint {
-	// inject our own log writer
-	v2applog.RegisterHandlerCreator(v2applog.LogType_Console,
-		func(lt v2applog.LogType,
-			options v2applog.HandlerCreatorOptions) (v2commlog.Handler, error) {
-			return v2commlog.NewLogger(createStdoutLogWriter()), nil
-		})
+    // inject our own log writer
+    v2applog.RegisterHandlerCreator(v2applog.LogType_Console,
+        func(lt v2applog.LogType,
+            options v2applog.HandlerCreatorOptions) (v2commlog.Handler, error) {
+            return v2commlog.NewLogger(createStdoutLogWriter()), nil
+        })
 
-	dialer := NewPreotectedDialer(s)
-	v2internet.UseAlternativeSystemDialer(dialer)
-	return &V2RayPoint{
-		SupportSet:   s,
-		dialer:       dialer,
-		AsyncResolve: adns,
-	}
+    dialer := NewProtectedDialer(s)
+    v2internet.UseAlternativeSystemDialer(dialer)
+    return &V2RayPoint{
+        SupportSet:   s,
+        dialer:       dialer,
+        AsyncResolve: adns,
+    }
 }
 
-/*
-CheckVersionX string
-This func will return libv2ray binding version and V2Ray version used.
-*/
+// CheckVersionX returns the version of libv2ray and Xray-core
 func CheckVersionX() string {
-	var version = 30
-	return fmt.Sprintf("Lib v%d, Xray-core v%s", version, v2core.Version())
+    var version = 30
+    return fmt.Sprintf("Lib v%d, Xray-core v%s", version, v2core.Version())
 }
 
 func measureInstDelay(ctx context.Context, inst *v2core.Instance, url string) (int64, error) {
-	if inst == nil {
-		return -1, errors.New("core instance nil")
-	}
+    if inst == nil {
+        return -1, errors.New("core instance nil")
+    }
 
-	tr := &http.Transport{
-		TLSHandshakeTimeout: 6 * time.Second,
-		DisableKeepAlives:   true,
-		DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
-			dest, err := v2net.ParseDestination(fmt.Sprintf("%s:%s", network, addr))
-			if err != nil {
-				return nil, err
-			}
-			return v2core.Dial(ctx, inst, dest)
-		},
-	}
+    tr := &http.Transport{
+        TLSHandshakeTimeout: 6 * time.Second,
+        DisableKeepAlives:   true,
+        DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
+            dest, err := v2net.ParseDestination(fmt.Sprintf("%s:%s", network, addr))
+            if err != nil {
+                return nil, err
+            }
+            return v2core.Dial(ctx, inst, dest)
+        },
+    }
 
-	c := &http.Client{
-		Transport: tr,
-		Timeout:   12 * time.Second,
-	}
+    c := &http.Client{
+        Transport: tr,
+        Timeout:   12 * time.Second,
+    }
 
-	if len(url) <= 0 {
-		url = "https://www.google.com/generate_204"
-	}
-	req, _ := http.NewRequestWithContext(ctx, "GET", url, nil)
-	start := time.Now()
-	resp, err := c.Do(req)
-	if err != nil {
-		return -1, err
-	}
+    if len(url) <= 0 {
+        url = "https://www.google.com/generate_204"
+    }
+    req, _ := http.NewRequestWithContext(ctx, "GET", url, nil)
+    start := time.Now()
+    resp, err := c.Do(req)
+    if err != nil {
+        return -1, err
+    }
 
-	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusNoContent {
-		return -1, fmt.Errorf("status != 20x: %s", resp.Status)
-	}
-	resp.Body.Close()
-	return time.Since(start).Milliseconds(), nil
+    if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusNoContent {
+        return -1, fmt.Errorf("status != 20x: %s", resp.Status)
+    }
+    resp.Body.Close()
+    return time.Since(start).Milliseconds(), nil
 }
 
-// This struct creates our own log writer without datatime stamp
+// This struct creates our own log writer without date/time stamp
 // As Android adds time stamps on each line
 type consoleLogWriter struct {
-	logger *log.Logger
+    logger *log.Logger
 }
 
 func (w *consoleLogWriter) Write(s string) error {
-	w.logger.Print(s)
-	return nil
+    w.logger.Print(s)
+    return nil
 }
 
 func (w *consoleLogWriter) Close() error {
-	return nil
+    return nil
 }
 
-// This logger won't print data/time stamps
+// This logger won't print date/time stamps
 func createStdoutLogWriter() v2commlog.WriterCreator {
-	return func() v2commlog.Writer {
-		return &consoleLogWriter{
-			logger: log.New(os.Stdout, "", 0)}
-	}
+    return func() v2commlog.Writer {
+        return &consoleLogWriter{
+            logger: log.New(os.Stdout, "", 0)}
+    }
 }
