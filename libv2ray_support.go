1d2c2ef6a54c5b895676a3d51d30f916862da0f2
gh88562 <196129267+gh88562@users.noreply.github.com>
2025-02-19 11:22:44 +0800

    revert 19486878e52ac8d166a7c8a6ebc2e1838641f6fe

--- libv2ray_support.go
+++ libv2ray_support.go
@@ -260,7 +260,7 @@ func (d *ProtectedDialer) Dial(ctx context.Context,
 	}
 
 	// v2ray connecting to "domestic" servers, no caching results
-	// log.Printf("Not Using Prepared: %s,%s", network, Address)
+	log.Printf("Not Using Prepared: %s,%s", network, Address)
 	resolved, err := d.lookupAddr(Address)
 	if err != nil {
 		return nil, err
@@ -302,7 +302,7 @@ func (d *ProtectedDialer) fdConn(ctx context.Context, ip net.IP, port int, netwo
 		}
 	} else {
 		if err := unix.Connect(fd, sa); err != nil {
-			// log.Printf("fdConn unix.Connect err, Close Fd: %d Err: %v", fd, err)
+			log.Printf("fdConn unix.Connect err, Close Fd: %d Err: %v", fd, err)
 			return nil, err
 		}
 	}
