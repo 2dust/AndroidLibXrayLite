5cdcbc611f5df8f33cb2040de5369f368e099e3e
2dust <31833384+2dust@users.noreply.github.com>
2025-05-07 10:38:55 +0800

    Update libv2ray_main.go

--- libv2ray_main.go
+++ libv2ray_main.go
@@ -190,7 +190,7 @@ func MeasureOutboundDelay(ConfigureFileContent string, url string) (int64, error
 
 // CheckVersionX returns the library and Xray versions
 func CheckVersionX() string {
-	var version = 31
+	var version = 32
 	return fmt.Sprintf("Lib v%d, Xray-core v%s", version, core.Version())
 }
 
